ce4d6936a03fa734fb91d45bab1b1ba9bb7b60c1
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2019-11-13 22:49:14 -0300

    init

---
+++ src/main/java/br/edu/gazin/bopepo/SpringBootBopepoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.gazin.bopepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBopepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBopepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/gazin/bopepo/SpringBootBopepoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.gazin.bopepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBopepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
